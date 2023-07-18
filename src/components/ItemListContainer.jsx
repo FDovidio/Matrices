3a988ec027caf6771d26286a2877ba24e87c7bd8
Francisco Dovidio <francisco.dovidio+1@gmail.com>
2023-07-17 22:52:24 -0300

    logo en header y colocar texto a la derecha

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { Flex, Heading } from '@chakra-ui/react'
+import React from "react";
+import { Flex, Heading, Image } from "@chakra-ui/react";
 
-const ItemListContainer = ({greeting}) => {
-
+const ItemListContainer = ({ greeting }) => {
   return (
-    <div className='header'>
-    <Heading  size='lg' fontSize='50px' >{greeting}</Heading>
-    <p className='headerParrafo'>El primer e-commerce con el mejor catalogo de productos que llevara tu experiencia de mate al siguiente nivel</p>
+    <div className="header">
+      <Image
+        src="https://cdn-icons-png.flaticon.com/512/5346/5346913.png"
+        alt="Dan Abramov"
+        boxSize="80px"
+      />
+      <Heading size="lg" fontSize="50px">
+        {greeting}
+      </Heading>
+      <p className="headerParrafo">
+        El primer e-commerce con el mejor catalogo de productos que llevara tu
+        experiencia de mate al siguiente nivel
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default ItemListContainer
+export default ItemListContainer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Menu,
   MenuButton,
@@ -11,42 +11,48 @@ import {
   Image,
   MenuDivider,
   MenuGroup,
-} from '@chakra-ui/react'
-import { ChevronDownIcon, HamburgerIcon} from '@chakra-ui/icons'
-import CardWidget from './CardWidget'
+} from "@chakra-ui/react";
+import { ChevronDownIcon, HamburgerIcon } from "@chakra-ui/icons";
+import CardWidget from "./CardWidget";
 
 const Navbar = () => {
   return (
-    <Flex bg='#9fac82' >
-          <Menu >
-  <MenuButton as={Button} leftIcon={<HamburgerIcon />} bg='#9fac82'  mt='2'>
-    Matrices
-  </MenuButton>
-  <MenuList>
-    <MenuGroup title='Productos'>
-      <MenuItem>Mates</MenuItem>
-      <MenuItem>Yerba</MenuItem>
-      <MenuItem>Bombillas</MenuItem>
-    </MenuGroup>
-    <MenuDivider/>
-    <MenuItem>Quienes Somos</MenuItem>
-    <MenuItem>Contacto</MenuItem>
-  </MenuList>
-</Menu>
+    <Flex bg="#9fac82">
+      <Menu>
+        <MenuButton
+          as={Button}
+          leftIcon={<HamburgerIcon />}
+          bg="#9fac82"
+          mt="2"
+        >
+          Matrices
+        </MenuButton>
+        <MenuList>
+          <MenuGroup title="Productos">
+            <MenuItem>Mates</MenuItem>
+            <MenuItem>Yerba</MenuItem>
+            <MenuItem>Bombillas</MenuItem>
+          </MenuGroup>
+          <MenuDivider />
+          <MenuItem>Quienes Somos</MenuItem>
+          <MenuItem>Contacto</MenuItem>
+        </MenuList>
+      </Menu>
+      <Spacer />
+      <Box>
+        <Image
+          src="https://cdn-icons-png.flaticon.com/512/5346/5346913.png"
+          alt="Dan Abramov"
+          boxSize="37px"
+          m="2"
+        />
+      </Box>
+      <Spacer />
+      <Box p="2">
+        <CardWidget />
+      </Box>
+    </Flex>
+  );
+};
 
-<Spacer/>
-<Box>
-<Image src='https://cdn-icons-png.flaticon.com/512/5346/5346913.png' alt='Dan Abramov' boxSize='37px' m='2' />
-
-</Box>
-<Spacer/>
-<Box p='2' >
-  <CardWidget/>
-</Box>
-</Flex>
-
-    
-  )
-}
-
-export default Navbar
+export default Navbar;
